909d4258a5fe06e95d2fe067ffad92a39e2849e1
ul0603 <82021108+ul0603@users.noreply.github.com>
2022-03-12 19:44:23 +0200

    Docstringit

--- palvelut.py
+++ palvelut.py
@@ -3,12 +3,24 @@ import random
 
 
 class Asiakas():
+    """Luokka, joka kuvaa Asiakkaan.
+    :ivar nimi: Asiakkaan nimi
+    :type nimi: str
+    :ivar ika: Asiakkaan ikä
+    :type ika: int
+    """
+    
     def __init__(self, nimi, ika):
+        """Konstruktori
+        """
         self.nimi= nimi
         self.ika = ika
 
     def get_nimi(self):
-        return self.nimi 
+        return self.nimi
+        """Getteri funktio joka palauttaa nimen
+        type nimi: str
+        """
 
     def set_nimi(self, uusinimi):
         try:
@@ -16,10 +28,15 @@ class Asiakas():
                 self.nimi= uusinimi
         except ValueError:
             raise ValueError("Kannattaa antaa uusi nimi")
+        """Setteri funktio joka asettaa nimen arvon tilalle uuden nimen
+        type uusinimi: str
+        """
         
 
     def get_ika(self):
         return self.ika
+        """Getteri funktio joka palauttaa Asiakkaan iän
+        type ika: str"""
 
     def set_ika(self, uusi_ika):
         try:
@@ -27,6 +44,8 @@ class Asiakas():
                 self.ika = uusi_ika
         except ValueError:
             raise ValueError("Kannattaa antaa uusi ikä")
+        """Setteri funktio joka asettaa iän arvon tilalle uuden iän
+        type uusi_ika: str"""
         
             
     
@@ -43,19 +62,28 @@ class Asiakas():
 
         asiakasnro = f'{nro1}{nro2}-{nro3}{nro4}{nro5}-{nro6}{nro7}{nro8}'
         return asiakasnro
+        """Funktio joka palauttaa 8 eri numeroa satunnaisesti 0 ja 9 väliltä
+        type nro1-nro8: int""" 
 
     def get_asiakasnro(self):
         return asiakasnro
+        """Getteri funktio joka palauttaa asiakasnumeron
+        type asiakasnro: str f"""
 
 
 
 class Palvelu():
     def __init__(self, tuotenimi, asiakkaat=[]):
+        """Konstruktori
+        """
         self.tuotenimi=tuotenimi
         self.asiakkaat=[]
 
-    def luo_asiakasrivi(Asiakas):
+    def __luo_asiakasrivi(self, Asiakas):
          print(f'{Asiakas.get_nimi()},{Asiakas.get_asiakasnro()}, on {Asiakas.get_ika()}-vuotias')
+         """Luo asiakasrivin käyttämällä get metodeja
+        Asiakas type: class
+        """
 
     def lisaa_asiakas(self, Asiakas):
         asiakas = input("Anna Asiakas: ")
@@ -64,6 +92,9 @@ class Palvelu():
                 self.asiakkaat.append(asiakas)
         except ValueError:
             raise ValueError ("Kannattaa antaa uusi asiakas")
+        """Lisää annetun asiakkaan asiakkaat listaan ja nostaa ValueErrorin jos ei anneta asiakasta
+        type asiakas:: str
+        """
             
 
     def poista_asiakas(self, Asiakas):
@@ -73,14 +104,20 @@ class Palvelu():
                 self.asiakkaat.remove(asiakas)
         except ValueError:
             raise ValueError ("Kannattaa antaa uusi asiakas")
+        """Poistaan annetun asiakkaan asiakaat listasta ja nostee ValueErrorin jos ei anneta asiakasta
+        type asiakas: str
+        """
 
     def tulosta_asiakkaat(self):
-        self.luo_asiakasrivi()
-
+        print self.luo_asiakasrivi()
+        """Tulostaa luodun asiakasrivin käyttämällä luo_asiakasrivi metodia
+        """
 
 
 class ParempiPalvelu(Palvelu):
     def __init__(self, tuotenimi, edut=[]):
+        """Konstruktori
+        """
         super().__init__(tuotenimi)
         self.edut=[]
 
@@ -91,6 +128,9 @@ class ParempiPalvelu(Palvelu):
                 self.edut.append(self, etu)
         except ValueError:
             raise ValueError ("Kannattaa antaa uusi etu")
+        """Lisää edun edut listaan ja jos ei anneta arvoa nostaa ValueErrorin
+        type etu: str
+        """
 
     def poista_etu(self, etu):
         etu = input("Anna Etu: ")
@@ -99,6 +139,12 @@ class ParempiPalvelu(Palvelu):
                 self.edut.remove(asiakas)
         except ValueError:
             raise ValueError ("Kannattaa antaa uusi etu")
+        """Poistaa edun edut listasta ja jos ei anneta arvoa nostaa ValueErrorin
+        type etu: str
+        """
 
     def tulosta_edut():
         print(edut)
+        """Tulostaa edut listan
+        type edut: list
+        """

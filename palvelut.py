7be902792ce7e291174dee89933909efae519b39
ul0603 <82021108+ul0603@users.noreply.github.com>
2022-05-19 20:41:30 +0300

    Asiakasnro on vieläkin tyhjä lista ja alkuun ei Tule Sammas niin kuin pitäisi
    Asiakasnro pitää korjata ettei se ole tyhjä lista tulostuksessa ja ValueError eli lisaa ja poista asiakkaat ja edut pitää kai korjata koskka loppuun ei tule Asiakas on annettava ja Uusi nimi on annettava.

--- palvelut.py
+++ palvelut.py
@@ -2,6 +2,7 @@ import random
 
 
 
+
 class Asiakas():
     """Luokka, joka kuvaa Asiakkaan.
     :ivar nimi: Asiakkaan nimi
@@ -13,12 +14,12 @@ class Asiakas():
     def __init__(self, nimi, ika):
         """Konstruktori
         """
-        self._nimi= str(nimi)
-        self._ika = int(ika)
-        self._asiakasnro = []
+        self.__nimi= str(nimi)
+        self.__ika = int(ika)
+        self.__asiakasnro = []
 
     def get_nimi(self):
-        return self._nimi
+        return self.__nimi
         """Getteri funktio joka palauttaa nimen
         type nimi: str
         """
@@ -26,7 +27,7 @@ class Asiakas():
     def set_nimi(self, uusinimi):
         try:
             if uusinimi != "":
-                self._nimi= uusinimi
+                self.__nimi= uusinimi
         except ValueError:
             raise ValueError("Kannattaa antaa uusi nimi")
         """Setteri funktio joka asettaa nimen arvon tilalle uuden nimen
@@ -35,14 +36,14 @@ class Asiakas():
         
 
     def get_ika(self):
-        return self._ika
+        return self.__ika
         """Getteri funktio joka palauttaa Asiakkaan iän
         type ika: str"""
 
     def set_ika(self, uusi_ika):
         try:
             if uusi_ika != "":
-                self._ika = uusi_ika
+                self.__ika = uusi_ika
         except ValueError:
             raise ValueError("Kannattaa antaa uusi ikä")
         """Setteri funktio joka asettaa iän arvon tilalle uuden iän
@@ -50,7 +51,7 @@ class Asiakas():
         
             
     
-    def _luo_nro(self):
+    def __luo_nro(self):
 
         nro1 = random.randint(0, 9)
         nro2 = random.randint(0, 9)
@@ -61,13 +62,13 @@ class Asiakas():
         nro7 = random.randint(0, 9)
         nro8 = random.randint(0, 9)
 
-        _asiakasnro = f'{nro1}{nro2}-{nro3}{nro4}{nro5}-{nro6}{nro7}{nro8}'
-        return _asiakasnro
+        self.__asiakasnro = f'{nro1}{nro2}-{nro3}{nro4}{nro5}-{nro6}{nro7}{nro8}'
+        return self.__asiakasnro
         """Funktio joka palauttaa 8 eri numeroa satunnaisesti 0 ja 9 väliltä
         type nro1-nro8: int""" 
 
     def get_asiakasnro(self):
-        return self._asiakasnro
+        return self.__asiakasnro
         """Getteri funktio joka palauttaa asiakasnumeron
         type asiakasnro: str f"""
 
@@ -78,7 +79,7 @@ class Palvelu():
         """Konstruktori
         """
         self.tuotenimi = str(tuotenimi)
-        self._asiakkaat = []
+        self.__asiakkaat = []
 
     def _luo_asiakasrivi(self, Asiakas):
          print(f'{Asiakas.get_nimi()},{Asiakas.get_asiakasnro()}, on {Asiakas.get_ika()}-vuotias')
@@ -91,7 +92,7 @@ class Palvelu():
         asiakas = Asiakas
         try:
             if asiakas != None:
-                self._asiakkaat.append(asiakas)
+                self.__asiakkaat.append(asiakas)
         except ValueError:
             raise ValueError ("Kannattaa antaa uusi asiakas")
         """Lisää annetun asiakkaan asiakkaat listaan ja nostaa ValueErrorin jos ei anneta asiakasta
@@ -103,15 +104,12 @@ class Palvelu():
         asiakas = input("Anna Asiakas: ")
         try:
             if asiakas != None:
-                self._asiakkaat.remove(asiakas)
-        except ValueError:
-            raise ValueError ("Kannattaa antaa uusi asiakas")
-        """Poistaan annetun asiakkaan asiakaat listasta ja nostee ValueErrorin jos ei anneta asiakasta
-        type asiakas: str
-        """
-
+                self.__asiakkaat.remove(asiakas)
+        except:
+            pass
+        
     def tulosta_asiakkaat(self):
-        for x in self._asiakkaat:
+        for x in self.__asiakkaat:
             self._luo_asiakasrivi(x)
         """Tulostaa luodun asiakasrivin käyttämällä luo_asiakasrivi metodia
         """
@@ -122,12 +120,12 @@ class ParempiPalvelu(Palvelu):
         """Konstruktori
         """
         super().__init__(tuotenimi)
-        self._edut = []
+        self.__edut = []
 
     def lisaa_etu(self, etu):
         try:
             if etu != None:
-                self._edut.append(etu)
+                self.__edut.append(etu)
         except ValueError:
             raise ValueError ("Kannattaa antaa uusi etu")
         """Lisää edun edut listaan ja jos ei anneta arvoa nostaa ValueErrorin
@@ -138,15 +136,14 @@ class ParempiPalvelu(Palvelu):
         etu = etu
         try:
             if etu != None:
-                self._edut.remove(etu)
+                self.__edut.remove(etu)
         except:
             pass
 
     def tulosta_edut(self):
         print("Tuotteen " + self.tuotenimi + " edut ovat:")
-        for etu in self._edut:
-            print(f'{etu}')
-        """Tulostaa edut listan
-        type edut: list
-        """
-
+        for etu in self.__edut:
+            print (f'{etu}')
+            """Tulostaa _edut listasta edut
+            type _edut: list
+            """
